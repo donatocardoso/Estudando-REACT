765658cfa1dd02d0aed06dda9756d5c043929dd1
Donato Cardoso <donato_cardoso@hotmail.com.br>
2018-06-25 23:19:12 -0300

    Corigindo estrutura do projeto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Controllers/App';
+import App from './JSX/App';
 import './Content/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
