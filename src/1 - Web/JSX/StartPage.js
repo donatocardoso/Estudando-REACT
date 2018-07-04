dc472d40b76ff2c58f8a1f9f151696a2867a939d
Donato Cardoso <donato.avila@consorcioluiza.com.br>
2018-07-04 09:04:31 -0300

    Adicionando imgs disabled do menu e estilisando os buttons disabled

--- src/1 - Web/JSX/StartPage.js
+++ src/1 - Web/JSX/StartPage.js
@@ -1,5 +1,6 @@
 import '../Content/Css/StartPage.css';
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 import MenuBrinde       from '../Content/Images/menuBrinde.png';
 import MenuParticipante from '../Content/Images/menuParticipante.png';
@@ -12,29 +13,30 @@ class StartPage extends Component {
 	render() {
 		return (
 			<div id='divStartPage'>
-				<div id='divCadBrinde'>
+				<button id='btnCadBrinde'>
                     <img src={MenuBrinde} alt='Menu Brinde' />
                     <span class='legend' onClick={() => RenderConfig.WebForms('/Views/Brinde.aspx')} >
                         Cadastrar Brindes
                     </span>
-                </div>
+                </button>
 
-                <div id='divCadParticipante' disabled>
+                <button id='btnCadParticipante' onClick={() => RenderConfig.React(<ReactPage />, '#conteudoReact')} disabled='disabled' >
                     <img src={MenuParticipante} alt='Menu Participante' />
-                    <span class='legend' onClick={() => RenderConfig.React(<ReactPage />, '#conteudoReact')} >
+                    <span class='legend' onClick={() => $('#btnCadParticipante').trigger('click')} >
                         Cadastrar Participantes
                     </span>
-                </div>
+                </button>
 
-                <div id='divCadPartida'>
+                <button id='btnCadPartida' onClick={() => RenderConfig.React(<ReactPage />, '#conteudoReact')} disabled='disabled' >
                     <img src={MenuPartida} alt='Menu Partida' />
-                    <span class='legend'>
+                    <span class='legend' onClick={() => $('#btnCadPartida').trigger('click')} >
                         Iniciar Partida
                     </span>
-                </div>
+                </button>
 			</div>
 		);
-	}
+    }
+
 
 }
 
