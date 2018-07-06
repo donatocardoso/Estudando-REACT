512e821a8ca09d7066c7b5d02e5ea09b1b61be4e
Donato Cardoso <donato.avila@consorcioluiza.com.br>
2018-07-06 11:55:17 -0300

    Renomeando arquivo de config da API

--- public/startproject.js
+++ public/startproject.js
@@ -1,7 +1,7 @@
 var cmd = require('node-cmd');
 
 console.log('Starting Api...');
-cmd.run('set PORT=3001 && node ./src/3-Api/Config/StartConfig.js');
+cmd.run('set PORT=3001 && node ./src/3-Api/Config/ApiConfig.js');
 
 console.log('Starting Web...');
 cmd.run('set PORT=3000 && react-scripts start');
